d003cd332724702e7883dd64dbf7909b02752203
ahmeddheesham <ahmeddheesham@gmail.com>
2024-05-26 16:10:19 +0300

    "refactor image upload method using Laravel intervention/image" / lesson 7

--- app/Http/Controllers/Dashboard/SettingController.php
+++ app/Http/Controllers/Dashboard/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Dashboard\SettingUpdateRequest;
 use App\Models\Setting;
+use App\Utils\ImageUpload;
 use Illuminate\Http\Request;
 use Image;
 use Storage;
@@ -14,32 +15,22 @@ class SettingController extends Controller
     public function index() {
         return view('dashboard.settings.index');
     }
-    public function update(SettingUpdateRequest $request ,Setting $setting) 
+    public function update(SettingUpdateRequest $request, Setting $setting)
     {
-        // dd($request->all());
         $setting->update($request->validated());
-        $imagename = date('Y-m-d') . '.' . $request->logo->extension();
 
 
-        $logo = Image::make($request->logo->path());
-        $logo->fit(200, 200, function ($constraint) {
-            $constraint->upsize();
-        })->stream();
+        if ($request->has('logo')) {
+            $logo = ImageUpload::uploadImage($request->logo, 100, 200, 'logo/');
+            $setting->update(['logo' => $logo]);
+        }
 
-        Storage::disk('public')->put($imagename, $logo);
-
-
-        $setting->update(['logo' => 'public/' . $imagename]);
-
-        // dd($logo);
+        if ($request->has('favicon')) {
+            $favicon = ImageUpload::uploadImage($request->favicon, 32, 32, 'favicon/');
+            $setting->update(['favicon' => $favicon]);
+        }
 
 
-
-
-
-
-
-
-        return redirect()->route('dashboard.settings.index')->with('success' , 'تم تحديث البيانات بنجاح');
+        return redirect()->route('dashboard.settings.index')->with('success', 'تم تحديث البيانات بنجاح');
     }
 }
