bba283746abadb04bddc5af68ac40a290f3d2e50
ahmeddheesham <ahmeddheesham@gmail.com>
2024-05-30 11:25:12 +0300

    start impleminting service pattern / lesson 10

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -3,150 +3,64 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\Categories\CategoryDelete;
 use App\Http\Requests\Dashboard\Categories\CategoryDeleteRequest;
 use App\Http\Requests\Dashboard\Categories\CategoryStoreRequest;
 use App\Http\Requests\Dashboard\Categories\CategoryUpdateRequest;
+use App\Models\Category;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
-use App\Models\Category;
-use Yajra\DataTables\Facades\DataTables;
-use Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
-
-
-
-
+use DataTables;
 class CategoryController extends Controller
 {
 
     private $categoryService;
-    //create constructor to bind CategoryService class
+
     public function __construct(CategoryService $categoryService)
     {
         $this->categoryService = $categoryService;
     }
-
-
-
-
-
     public function index()
     {
         $mainCategories = $this->categoryService->getMainCategories();
-        return view('dashboard.categories.index', compact('mainCategories'));
+        return view('dashboard.categories.index' , compact('mainCategories'));
     }
-
-
-
-
 
 
     public function getall()
     {
-        $query = Category::select('*')->with('parent');
-        return DataTables::of($query)
-        ->addIndexColumn()
-        ->addColumn('action', function ($row) {
-            return $btn = '
-            <a 
-            href="'.Route('dashboard.categories.edit', $row->id) .'"  
-            class="edit btn btn-success btn-sm "> 
-            <i class="fa fa-edit"></i> 
-            </a>
-            
-            <button 
-            type="button" 
-            id="deleteBtn" 
-            data_id="'. $row->id .'"  
-            class= "btn btn-danger mt-md-0 mt-2"  
-            data-bs-toggle="modal"
-            data-original-title="test"
-            data-bs-target="#deletemodal"> 
-            <i class = "fa fa-tarsh"></i> 
-            </button>' ;
-        })
-
-        ->addcolumn('parent', function($row) {
-            return($row->parent == 0 ) ? 'قسم رئيسي'  : $row->parents->name;
-        })
-
-        ->addcolumn('image', function($row) {
-            return '<img src="' .asset($row->image) . '"  width="100px"  height="100px"  >';
-        })
-
-        ->rawColumns(['parent', 'action', 'image'])
-        ->make(true);
+       return $this->categoryService->datatable();
     }
-        
-            
+
    
-
-    /**
-     * Show the form for creating a new resource.
-     *
-    //  * @return \Illuminate\Http\Response
-     */
-    public function create(){
-        
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $this->categoryService->store($request->validated());
+        return redirect()->route('dashboard.categories.index')->with('success', 'تمت الاضافة بنجاح');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-     */
+   
+    
     public function edit($id)
     {
-        $category = $this->categoryService->getById($id, true);
+        $category = $this->categoryService->getById($id,true);
         $mainCategories = $this->categoryService->getMainCategories();
-        return view('dashboard.categories.edit', compact('category', 'mainCategories'));
+        return view('dashboard.categories.edit' , compact('category','mainCategories'));
     }
 
-
-
-
-
-    public function update($id, CategoryUpdateRequest $request)
+   
+    public function update( $id ,CategoryUpdateRequest $request)
     {
-        $this->categoryService->update($id, $request->validated());
-        return redirect()->route('dashboard.categories.edit', $id)->with('success', 'تمت الاضافة بنجاح');
+        $this->categoryService->update($id,$request->validated());
+        return redirect()->route('dashboard.categories.edit' , $id)->with('success', 'تمت الاضافة بنجاح');
     }
 
-
-
-
-
-
-
-
+    
 
 
     public function delete(CategoryDeleteRequest $request)
     {
-        Category::whereId($request->id)->delete();
-        return redirect()->route('dashboard.categories.index');
+     Category::whereId($request->id)->delete();
+     return redirect()->route('dashboard.categories.index');
     }
 }
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\Category;
 use App\Utils\ImageUpload;
 use Yajra\DataTables\Facades\DataTables;
@@ -17,12 +18,12 @@ class CategoryService
     public function store($params)
     {
         $params['parent_id'] = $params['parent_id'] ?? 0;
-        if(isset($params['image'])) {
+        if (isset($params['image'])) {
             $params['image'] = ImageUpload::uploadImage($params['image']);
         }
         return Category::create($params);
     }
-    
+
 
 
 
@@ -30,7 +31,7 @@ class CategoryService
     public function getById($id, $childrenCount = false)
     {
         $query = Category::where('id', $id);
-        if($childrenCount){
+        if ($childrenCount) {
             $query->withCount('child');
         }
         return $query->firstOrFail();
@@ -43,7 +44,7 @@ class CategoryService
         // dd($params);
         $category = $this->getById($id);
         $params['parent_id'] = $params['parent_id'] ?? 0;
-        if(isset($params['image'])) {
+        if (isset($params['image'])) {
             $params['image'] = ImageUpload::uploadImage($params['image']);
         }
         return $category->update($params);
@@ -55,13 +56,12 @@ class CategoryService
 
 
 
-    public function datatable() 
+    public function datatable()
     {
         $query = Category::select('*')->with('parent');
-        return  DataTables::of($query)
+        return DataTables::of($query)
             ->addIndexColumn()
-            ->addColumn('action', function ($row) 
-            {
+            ->addColumn('action', function ($row) {
                 return $btn = '
                         <a href="' . Route('dashboard.categories.edit', $row->id) . '"
                           class="edit btn btn-success btn-sm" >
@@ -82,26 +82,23 @@ class CategoryService
 
 
 
+            ->addcolumn('parent', function ($row) {
 
-        
+                if ($row->parent) {
+                    return $row->parent->name;
+                }
+                return 'قسم رئيسي';
 
-        ->addcolumn('parent', function($row) {
+                // return($row->parent == 0 ) ? 'قسم رئيسي'  : $row->parents->name;
+    
+            })
 
-             if($row->parent){
-                return $row->parent->name;
-             }
-             return 'قسم رئيسي';
-             
-            // return($row->parent == 0 ) ? 'قسم رئيسي'  : $row->parents->name;
+            ->addcolumn('image', function ($row) {
+                return '<img src="' . asset($row->image) . '"  width="100px"  height="100px"  >';
+            })
 
-        })
-
-        ->addcolumn('image', function($row) {
-            return '<img src="' .asset($row->image) . '"  width="100px"  height="100px"  >';
-        })
-
-        ->rawColumns(['parent', 'action', 'image'])
-        ->make(true);
+            ->rawColumns(['parent', 'action', 'image'])
+            ->make(true);
     }
 
 
